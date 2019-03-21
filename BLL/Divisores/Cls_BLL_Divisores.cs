cdf4c4bb3966020c20ff8763fe48e28d709e4d07
Steven Rojas <Steven Rojas@SROJAS-X1>
2019-03-20 22:34:09 -0600

    Cls creation for DAL and BLL

---
+++ BLL/Divisores/Cls_BLL_Divisores.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Divisores
+{
+  public  class Cls_BLL_Divisores
+    {
+    }
+}
---
+++ BLL/Fibonacci/Cls_BLL_Fibonacci.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Fibonacci
+{
+   public class Cls_BLL_Fibonacci
+    {
+    }
+}
